3f9093249127e37ac903bc746bf3ad172908adf5
totang <totang@ucsd.edu>
2017-06-06 12:18:36 -0700

    added access control

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 var api = require('./API.js');
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header('Access-Control-Allow-Methods','GET');
+  res.header('Access-Control-Allow-Headers','Content-Type');
+  next();
+});
 app.get('/list', api.root);
 app.get('/list/:type',api.listAffliction);
 app.get('/list/:type/:affliction',api.listAfflictionDistrict);
