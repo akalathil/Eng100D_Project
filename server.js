4a5c25e62d178dd48306f84e8ab181e0416db15a
totang <totang@ucsd.edu>
2017-06-05 23:20:14 -0700

    initial modified server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var api = require('./API.js');
+app.get('/list', api.root);
+app.get('/list/:type',api.listAffliction);
+app.get('/list/:type/:affliction',api.listAfflictionDistrict);
+app.get('/info/:type/:affliction/:district',api.getInfo);
+app.get('/data/:type/:affliction/',api.getData);
+app.get('/data/:type/:affliction/rows',api.getDataRows);
+app.get('/data/:type/:affliction/cols',api.getDataCols);
 app.listen(port);
 
 console.log('Magic happens on ' + port);
