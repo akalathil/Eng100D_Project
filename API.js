4305b61d44ca4cbcf2fa0886be19e1c11d595f64
totang <totang@ucsd.edu>
2017-06-10 03:45:13 -0700

    merged and added new routes plus POST requests

--- API.js
+++ API.js
@@ -1,5 +1,23 @@
 var data = require("./web/data.json");
 module.exports={
+	initial: function(){
+		var d=new Date();
+		var n=d.getTime();
+	  	for(type in data){
+		    for (affliction in data[type]){
+		      data[type][affliction]["info"]={
+		        "name":affliction,
+		        "date":n,
+		        "views":0,
+		        "description":affliction
+		      };
+		    }
+	  	};
+	  	console.log("HELLO");
+	},
+	all:function(req,res){
+		res.json(data);
+	},
 	root: function (req, res) {
 		response=[];
 		for (key in data){
@@ -15,15 +33,15 @@ module.exports={
 		for (key in data){
 			for (affliction in data[key]){
 				//console.log(key);
+				var temp=data[key][affliction]["info"]
 	    		response.push(
 	    			{
 	    				"type":key,
 	    				"affliction":affliction,
-	    				"name":affliction,
-	    				"date":n,
-	    				"views":0,
-	    				"description":affliction,
-	    				"tags":[]
+	    				"name":temp["name"],
+	    				"date":temp["date"],
+	    				"views":temp["views"],
+	    				"description":temp["description"]
 	    			}
 	    		);
 			}
@@ -39,21 +57,21 @@ module.exports={
 	  	}
 	  	res.json(response);
 	},
-	listAfflictionDistrict: function(req,res){
+	listAfflictionProfile: function(req,res){
 		var type=String(req.params.type);
 		var affliction=String(req.params.affliction);
 		response=[];
-		for (key in data[type][affliction]){
+
+		/*for (key in data[type][affliction]){
 	    	response.push(key);
-	  	}
-	  	res.json(response);
+	  	}*/
+	  	res.json(data[type][affliction]);
 	},
 	getInfo: function(req,res){
-		//needs type, affliction,district
+		//needs type, affliction
 		var type= String(req.params.type);
 		var affliction=String(req.params.affliction);
-		var district=String(req.params.district);
-		res.json(data[type][affliction][district]["info"]);
+		res.json(data[type][affliction]["info"]);
 	},
 	getData: function(req,res){
 		//needs type, affliction,district
@@ -70,6 +88,122 @@ module.exports={
 		var type= String(req.params.type);
 		var affliction=String(req.params.affliction);
 		res.json(data[type][affliction]["Data"]["cols"]);
+	},
+	editCols: function(req,res){
+		/*
+		{
+			[col1,col2,col3...]
+		}
+		*/
+		var type= String(req.params.type);
+		var affliction=String(req.params.affliction);
+		data[type][affliction]["Data"]["cols"]=req.body;
+		res.send("SUCCESS");
+	},
+	editRows: function(req,res){
+		/*
+			{			
+				"0": Name
+				"1": Year
+				"2": Value
+				"index":0
+			}
+		*/
+		var type= String(req.params.type);
+		var affliction=String(req.params.affliction);
+		var index=req.body.index;
+		var temp=JSON.parse(data[type][affliction]["Data"]["rows"]);
+		temp[index]={
+						"Latitude":temp[index].Latitude,
+						"Longitude": temp[index].Longitude
+					};
+		for (key in req.body){
+			if (key!="index" && !isNaN(key)){
+				temp[index][key]=req.body[key]
+			}
+		}
+		data[type][affliction]["Data"]["rows"]=JSON.stringify(temp);
+		res.send(req.body);
+	},
+	editInfo: function(req,res){
+		/*
+		{
+			"name":"X",
+			"date":Y,
+			"views":Z,
+			"description":"new description"
+		}
+		*/
+		var type= String(req.params.type);
+		var affliction=String(req.params.affliction);
+		data[type][affliction]["info"]={
+			"name":req.body.name,
+			"date":req.body.date,
+			"views":req.body.views,
+			"description":req.body.description
+		};
+		res.json(data[type][affliction]["info"]);
+	},
+	addRow: function(req,res){
+		/*
+		{
+			"0":Name,
+			"1":Year,
+			"2":Value,
+			"Latitude":Lat,
+			"Longitude":Long
+		}
+		*/
+		var type= String(req.params.type);
+		var affliction=String(req.params.affliction);
+		tempData={};
+		for (key in req.body){
+			if (key=="Longitude"||key=="Latitude" || !isNaN(key)){
+				tempData[key]=req.body[key];
+			}
+		}
+		temp=JSON.parse(data[type][affliction]["Data"]["rows"]);
+		temp.push(tempData);
+		data[type][affliction]["Data"]["rows"]=JSON.stringify(temp);
+		res.send("SUCCESSFUL ADD");
+	},
+	addAffliction: function(req,res){
+		/*
+		{
+		    "name": Name,
+		    "description": description
+		}
+		*/
+		var d=new Date();
+		var n=d.getTime();
+		var type= String(req.params.type);
+		temp={
+			"Data":{
+				"cols":[],
+				"rows":[]
+			},
+			"info": {
+			    "name": req.body.name,
+			    "date": n,
+			    "views": 0,
+			    "description": req.body.description
+			}	
+		};
+		data[type][req.body.name]=temp;
+		res.send("SUCCESSFUL ADD");
+	},
+	changeType: function(req,res){
+		/*
+		{
+			"type": new type
+		}
+		*/
+		var type= String(req.params.type);
+		var affliction=String(req.params.affliction);
+		var newtype=String(req.body.type);
+		data[newtype][affliction]=data[type][affliction];
+		delete data[type][affliction];
+		res.send("SUCCESSFUL CHANGE");
 	}
 	
 }
--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 var api = require('./API.js');
+var data=require("./web/data.json");
+
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin","*");
-  res.header('Access-Control-Allow-Methods','GET');
+  res.header('Access-Control-Allow-Methods','GET,POST');
   res.header('Access-Control-Allow-Headers','Content-Type');
   next();
 });
+api.initial();
+app.get('/all', api.all);
+
 app.get('/list', api.root);
 app.get('/list/all',api.listall);
 app.get('/list/:type',api.listAffliction);
-app.get('/list/:type/:affliction',api.listAfflictionDistrict);
-app.get('/info/:type/:affliction/:district',api.getInfo);
+app.get('/list/:type/:affliction',api.listAfflictionProfile);
+
+app.get('/info/:type/:affliction',api.getInfo);
+
 app.get('/data/:type/:affliction/',api.getData);
 app.get('/data/:type/:affliction/rows',api.getDataRows);
 app.get('/data/:type/:affliction/cols',api.getDataCols);
+
+app.post('/edit/cols/:type/:affliction',api.editCols);
+app.post('/edit/rows/:type/:affliction',api.editRows);
+app.post('/edit/info/:type/:affliction',api.editInfo);
+app.post('/edit/type/:type/:affliction',api.changeType);
+app.post('/add/row/:type/:affliction', api.addRow);
+app.post('/add/affliction/:type',api.addAffliction);
+
+
 app.listen(port);
 
 console.log('Magic happens on ' + port);
